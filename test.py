54ea9d820799b4a71e136027c70dca0681102352
Lv1g1 <luigi.inguaggiato@gmail.com>
2024-03-03 23:49:27 +0100

    added test files

---
+++ test.py
@@ -0,0 +1,15 @@
+import os, time
+
+input = "test/open_{}.txt"
+check_output = "test/open_{}.output.txt"
+output = "output.txt"
+
+
+command = "./f < " + input + " > " + output
+diff = "diff " + check_output + " " + output
+for i in range(1,112):
+    os.system(command.format(i,i))
+    os.system(diff.format(i))
+    print(i)
+
+
